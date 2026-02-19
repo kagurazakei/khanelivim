5247f857200f05936d952b8c73a6c6188a5e6161
kagurazakei <maotsugiri@gmail.com>
2026-02-19 13:45:47 +0630

    🚀 update: default.nix for fail when using with npins

--- flake/default.nix
+++ flake/default.nix
@@ -4,46 +4,61 @@
   self,
   ...
 }:
+
 {
+  # ------------------------
+  # Imports
+  # ------------------------
   imports = [
     ./apps
     ./nixvim.nix
     ./overlays.nix
     ./pkgs-by-name.nix
-    inputs.flake-parts.flakeModules.partitions
+
+    # Import partitions as a set, do NOT call it
+    (import inputs.flake-parts.flakeModules.partitions)
   ];
 
+  # ------------------------
+  # Partitions
+  # ------------------------
   partitions = {
     dev = {
       module = ./dev;
-      extraInputsFlake = ./dev;
+
+      # If ./dev expects args, import it as a set
+      extraInputsFlake = (import ./dev { inherit inputs self lib; });
     };
   };
 
-  # Specify which outputs are defined by which partitions
+  # ------------------------
+  # Partitioned outputs
+  # ------------------------
   partitionedAttrs = {
     checks = "dev";
     devShells = "dev";
     formatter = "dev";
   };
 
+  # ------------------------
+  # Per-system configuration
+  # ------------------------
   perSystem =
-    {
-      config,
-      system,
-      ...
-    }:
-    {
-      _module.args.pkgs = import inputs.nixpkgs {
+    { config, system, ... }:
+    let
+      pkgs = import inputs.nixpkgs {
         inherit system;
         overlays = lib.attrValues self.overlays;
         config = {
           allowUnfree = true;
-          # FIXME: breaks git-hooks-nix installation
-          # allowAliases = false;
+          # allowAliases = false; # Uncomment if needed
         };
       };
+    in
+    {
+      _module.args.pkgs = pkgs;
 
+      # Reference your main package here
       packages.default = config.packages.khanelivim;
     };
 }
